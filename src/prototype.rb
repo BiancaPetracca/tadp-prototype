f0bdfc5fcf580a058f3eb8c40e137cdd3f86418b
Bianca Petracca <biancapetracca@gmail.com>
2016-04-28 23:42:40 -0300

    Implementing prototype by updating changes in all prototyped objects

--- src/prototype.rb
+++ src/prototype.rb
@@ -8,6 +8,7 @@ module Prototype
   where.singleton_class.send :attr_accessor, symbol
   where.send "#{symbol}=", value
   end
+
   def set_method(symbol, behavior)
     ## converting proc to block again
     define_singleton_method symbol, &behavior
@@ -23,14 +24,9 @@ module Prototype
     @its_prototype = a_proto
   end
 
-
-
+  
   def search_prototyped(some_object)
     some_object.respond_to?(:its_prototype=) and @its_prototype.eql? self
-  end
-
-  def get_lambda_method(symbol)
-    method symbol.to_proc
   end
 
 end
@@ -40,17 +36,4 @@ class PrototypedObject
   # every prototypedObject knows who is its prototype.
   attr_accessor :its_prototype
 
-
-  def method_missing(symbol, *args)
-    throw NoMethodError unless @its_prototype.respond_to? symbol
-    method = @its_prototype.get_lambda_method symbol
-    instance_exec &method
-  end
-  # en vez de que cuando se clone se seteen todos los metodos del prototype, podemos redefinir method missing
-  # y que se creen al momento de usarse, o sino que cuando todos los que tienen en @its_prototype a ese proto,
-  # definan el metodo que se esta creando en its prototype, sin method_missing. :)
-  # o tambien en vez de definirlo, podria bindear ese metodo al objeto actual (?)
-
-  ## al lambda habria que pasarle de parametro al self, que es el objeto en el que quiero hacer el instance_exec
-  ## de esa forma el lambda recibe un self que es este objeto 
   end
