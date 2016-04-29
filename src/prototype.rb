3051b15eaec218d5fd4576f690b5ebe7f6f5ede4
Bianca Petracca <biancapetracca@gmail.com>
2016-04-28 23:40:15 -0300

    Implementing prototype with method missing

--- src/prototype.rb
+++ src/prototype.rb
@@ -1,21 +1,12 @@
 module Prototype
   def set_property(symbol, value)
-    set_property_in_place self, symbol, value
-    prototyped_from_me.each do |place| set_property_in_place place, symbol, value end
+    self.singleton_class.send :attr_accessor, symbol
+    self.send "#{symbol}=", value
   end
 
-  def set_property_in_place(where, symbol, value)
-  where.singleton_class.send :attr_accessor, symbol
-  where.send "#{symbol}=", value
-  end
   def set_method(symbol, behavior)
     ## converting proc to block again
-    define_singleton_method symbol, &behavior
-    prototyped_from_me.each do |a_prototyped| a_prototyped.define_singleton_method symbol, &behavior end
-  end
-
-  def prototyped_from_me
-    Object.constants.select do |a_prototyped| search_prototyped a_prototyped end
+    self.define_singleton_method symbol, &behavior
   end
 
   ## es como un clone pero tuneado (?)
@@ -23,14 +14,8 @@ module Prototype
     @its_prototype = a_proto
   end
 
-
-
-  def search_prototyped(some_object)
-    some_object.respond_to?(:its_prototype=) and @its_prototype.eql? self
-  end
-
   def get_lambda_method(symbol)
-    method symbol.to_proc
+    self.method(symbol).to_proc
   end
 
 end
@@ -44,13 +29,9 @@ class PrototypedObject
   def method_missing(symbol, *args)
     throw NoMethodError unless @its_prototype.respond_to? symbol
     method = @its_prototype.get_lambda_method symbol
-    instance_exec &method
-  end
-  # en vez de que cuando se clone se seteen todos los metodos del prototype, podemos redefinir method missing
-  # y que se creen al momento de usarse, o sino que cuando todos los que tienen en @its_prototype a ese proto,
-  # definan el metodo que se esta creando en its prototype, sin method_missing. :)
-  # o tambien en vez de definirlo, podria bindear ese metodo al objeto actual (?)
+    self.instance_eval &method if @its_prototype.method(symbol).arity.eql? 0
+    self.instance_exec args, &method
 
-  ## al lambda habria que pasarle de parametro al self, que es el objeto en el que quiero hacer el instance_exec
-  ## de esa forma el lambda recibe un self que es este objeto 
   end
+
+end
